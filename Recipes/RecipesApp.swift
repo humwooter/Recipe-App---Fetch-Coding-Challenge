4ddbed4f19d561ed741d2c73d38265a1500c8607
Katya Raman <katyaraman@icloud.com>
2024-08-08 17:25:49 -0700

    first commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Katyayani G. Raman on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
